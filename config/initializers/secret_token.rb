414129c5aed88c2c1ba8b43d3fde706c4a92215d
Darren Yin <darrenyin0110@yahoo.com>
2013-02-27 04:52:45 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogblah::Application.config.secret_token = '49fd082bfce4b564f390b6388ab0e8e8849036eee2b9a117b1c811dfbd713a6afee503f8c15d9410b22ff799189f854e8bf49de3bcbb2dba86a498ed902a0cfd'
